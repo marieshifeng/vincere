21d5b1a82ce758655b16151d0af3dbcd27e60f37
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2014-05-25 16:20:19 -0700

    Fixed some drawing + audio bugs

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -8,7 +8,7 @@ https://github.com/byrichardpowell/draw
 
 // Initialise Socket.io
 var socket = io.connect('/');
-scoket.emit('user:drawingconnected');
+socket.emit('user:drawingconnected');
 
 // Random User ID
 // Used when sending data
