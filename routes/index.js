62f50ac71705774f8ff09a583debd63c1176ceeb
Marie Feng <marie.feng@gmail.com>
2014-06-03 22:12:57 -0700

    cleaned up, adding modal

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,3 @@
-var fs = require('fs');
-var sys = require('sys');
-var Image = require('../models/image');
-
 exports.index = function(req, res){
-  Image.find( function ( err, images ){
-       res.render('index', { title: "", images: images });           
-  });
+	res.render('index', { title: "" });           
 };
-
-exports.post = function(req, res){
-  var img = req.body.img;
-  var data = img.replace(/^data:image\/\w+;base64,/, "");
-  var buf = new Buffer(data, 'base64');
-  var randNum = Math.floor((Math.random() * 100) + 1);
-  //var filename = './public/images/temp/image' + randNum + '.png';
-  var filename = './public/images/image' + randNum + '.png';
-  var newImage = new Image();
-  //newImage.url = 'images/temp/image' + randNum + '.png';
-  newImage.url = 'images/image' + randNum + '.png';
-  newImage.save(); 
-  fs.writeFile(filename, buf, function (err, data) {
-    if (err) return console.log(err);
-  });
-  res.redirect('/#gallery');
-}
